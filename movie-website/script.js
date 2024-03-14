72cb037cfb7e02c03e75e2cb008141995c3e6ebd
Mehmet Çevik <mehmet_cvk20@outlook.com>
2024-03-14 17:11:25 +0300

    add slider js and project end

--- movie-website/script.js
+++ movie-website/script.js
@@ -2,16 +2,18 @@ const arrows = document.querySelectorAll(".arrow");
 const movieLists = document.querySelectorAll(".movie-list");
 
 arrows.forEach((arrow, i) => {
+  const widthRatio = Math.floor(window.innerWidth / 300);
+  const itemNumber = movieLists[i].querySelectorAll("img").length;
   let clickCounter = 0;
-  const imageItem = movieLists[i].querySelectorAll("img").length;
   arrow.addEventListener("click", function () {
     clickCounter++;
-    if (imageItem - (5 + clickCounter) >= 0) {
+    if (itemNumber - (4 + clickCounter) + (4 - widthRatio) >= 0) {
       movieLists[i].style.transform = `translateX(${
         movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
       }px)`;
     } else {
-      movieLists[i].style.transform = "translateX(0)";
+      movieLists[i].style.transform = `translateX(0)`;
+      clickCounter = 0;
     }
   });
 });
